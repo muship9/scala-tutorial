d79f0f7f3ec0fd9ca312f829e4366c88002a2ac2
shinp09 <57585657+shinp09@users.noreply.github.com>
2022-11-20 12:42:21 +0900

    feat: scalafmt

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # scala-tutorial
+Scala Docs
+https://docs.scala-lang.org/scala3/book/taste-intro.html
+
+Scala Text
 https://scala-text.github.io/scala_text/
---
+++ sandbox/main.scala
@@ -0,0 +1,10 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    //    immutable
+    val a = 0
+    //    mutable
+    var b = 0
+    a = 2 // compaire error
+    b = 1 // success
+  }
+}
